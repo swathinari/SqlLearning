eab4752d0b13d11701a679c79f60c9a2560de42e
swathinari <swathicomputer0128@gmail.com>
2025-06-14 17:33:54 +0530

    basic sql commands

---
+++ sqlBasic.sql
@@ -0,0 +1,17 @@
+Create database Online_business;
+use Online_business;
+show databases;
+
+use joins;
+show tables;
+desc products;
+
+insert into products values(204,"mouse","zebranix");
+select * from products;
+
+
+
+
+
+
+
